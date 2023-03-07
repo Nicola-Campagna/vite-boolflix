93b854bc403eb10a3915e5769194b2272954a2d2
Nicola-Campagna <campagnanicola379@gmail.com>
2023-03-07 17:42:01 +0100

    milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,64 @@
 <script>
 // import MyComponent
 
-// HEADER
-import AppHeader from "./components/AppHeader.vue"
+// SEARCHBAR
+import SearchBar from "./components/SearchBar.vue";
+
+// axios
+import axios from "axios"
 
 
 
 export default {
   data() {
     return {
+      appname: "BOOLFIX",
+      baseUri: "https://api.themoviedb.org/3",
+      apiKey: "7a133e976acaa1dadd272f9136c06752",
+      filmList: [],
     }
   },
 
   components: {
-    AppHeader,
-
+    // registro i compoknenti per utilizzarli
+    SearchBar,
 
   },
+
+  methods: {
+    fetchMovies(query) {
+      // console.log(query);
+      axios
+
+        .get(`${this.baseUri}/search/movie`, {
+
+          params: {
+            api_key: this.apiKey, query
+          }
+        })
+
+        .then((response) => {
+          console.log(response.data.results);
+          this.filmList = response.data.results
+        })
+
+    },
+  }
+
+
 };
 </script>
 
 <template>
-  <!-- HEADER -->
-  <AppHeader />
+  <SearchBar :title="appname" @search="fetchMovies" />
+  <h2>FILM:</h2>
+  <ul v-for="film in filmList">
+    <li>{{ film.original_title }}</li>
+    <li>{{ film.title }}</li>
+    <li>{{ film.vote_average }}</li>
+    <li>{{ film.original_language }}</li>
 
-  <!-- MAIN -->
+  </ul>
 </template>
 
 <style lang="scss"></style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,47 @@
+<script>
+
+export default {
+    data() {
+        return {
+            return: {
+                // termine da cercare
+                term: "",
+            }
+        }
+    },
+    props: {
+        title: String,
+    },
+    emits: ['search'],
+};
+</script>
+
+<template>
+    <header>
+        <div class="nav-head container-fluid">
+            <div class="row">
+                <div class="col">
+                    <nav class="navbar bg-body-tertiary">
+                        <div class="container-fluid">
+                            <!-- cambiare in logo succ.... -->
+                            <a class="navbar-brand">{{ title }}</a>
+                            <form class="d-flex" role="search" @submit.prevent="$emit('search', term)">
+
+                                <input v-model="term" class="form-control me-2" type="search"
+                                    placeholder="Search movies or tv series" aria-label="Search">
+
+
+
+                                <button class="btn btn-outline-success" type="submit" @click="$emit('search', term)">
+                                    Search
+                                </button>
+                            </form>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped></style>
